de29ed0a870d36d15c758431c43722dbd3c9e001
balaji <balaji_ramasamy@yahoo.com>
2017-04-20 21:30:50 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+# skill-nodejs-countrycapitaltrivia
+
+This game test your skill on geographical information. It will ask you questions on capital of all countries in the world. It will ask you 10 multiple choice questions about a country. Respond with the number of the answer.
+
+## Usage
+
+
+
+
+
+### Tools
+
+Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
+ ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
+
+Nodeclipse is free open-source project that grows with your contributions.
---
+++ src/countries_junior.js
@@ -0,0 +1,304 @@
+'use strict';
+
+module.exports = {
+	/**
+	 * When editing your questions pay attention to your punctuation. Make sure
+	 * you use question marks or periods. Make sure the first answer is the
+	 * correct one. Set at least ANSWER_COUNT answers, any extras will be
+	 * shuffled in.
+	 */
+	"COUNTRIES" : [
+			{
+				"What is the Capital of Afghanistan?" : [ "Kabul", "Herat",
+						"Kandahar", "Mazar-i-Sharif" ]
+			},
+			{
+				"What is the Capital of Argentina?" : [ "Buenos Aires",
+						"Rosario", "Córdoba", "Mendoza" ]
+			},
+			{
+				"What is the Capital of Australia?" : [ "Canberra", "Sydney",
+						"Melbourne", "Perth" ]
+			},
+			{
+				"What is the Capital of Austria?" : [ "Vienna", "Linz", "Graz",
+						"Salzburg" ]
+			},
+			{
+				"What is the Capital of Bangladesh?" : [ "Dhaka", "Khulna",
+						"Chittagong", "Rajshahi" ]
+			},
+			{
+				"What is the Capital of Belgium?" : [ "Brussels", "Ghent",
+						"Antwerp", "Charleroi" ]
+			},
+			{
+				"What is the Capital of Brazil?" : [ "Brasília", "São Paulo",
+						"Rio de Janeiro", "Salvador" ]
+			},
+			{
+				"What is the Capital of Canada?" : [ "Ottawa", "Toronto",
+						"Montreal", "Quebec City" ]
+			},
+			{
+				"What is the Capital of Chile?" : [ "Santiago", "Antofagasta",
+						"Puente Alto", "Valparaíso" ]
+			},
+			{
+				"What is the Capital of China?" : [ "Beijing", "Shanghai",
+						"Tianjin", "Guangzhou" ]
+			},
+			{
+				"What is the Capital of Taiwan?" : [ "Taipei", "Taichung",
+						"Kaohsiung", "Tainan City" ]
+			},
+			{
+				"What is the Capital of Colombia?" : [ "Bogotá",
+						"Santiago de Cali", "Medellín", "Barranquilla" ]
+			},
+			{
+				"What is the Capital of Costa Rica?" : [ "San José",
+						"Puerto Limón", "Alajuela", "San Francisco" ]
+			},
+			{
+				"What is the Capital of Cuba?" : [ "Havana", "Camagüey",
+						"Santiago de Cuba", "Holguín" ]
+			},
+			{
+				"What is the Capital of Egypt?" : [ "Cairo", "Giza",
+						"Alexandria", "Port Said" ]
+			},
+			{
+				"What is the Capital of Finland?" : [ "Helsinki", "Tampere",
+						"Espoo", "Vantaa" ]
+			},
+			{
+				"What is the Capital of France?" : [ "Paris", "Lyon",
+						"Marseille", "Toulouse" ]
+			},
+			{
+				"What is the Capital of Germany?" : [ "Berlin", "Munich",
+						"Hamburg", "Cologne" ]
+			},
+			{
+				"What is the Capital of Greece?" : [ "Athens", "Pátrai",
+						"Thessaloniki", "Piraeus" ]
+			},
+			{
+				"What is the Capital of Hong Kong?" : [ "Victoria", "Kowloon",
+						"Victoria", "Tsuen Wan" ]
+			},
+			{
+				"What is the Capital of Hungary?" : [ "Budapest", "Miskolc",
+						"Debrecen", "Szeged" ]
+			},
+			{
+				"What is the Capital of India?" : [ "New Delhi", "Mumbai",
+						"Chennai", "Bangalore" ]
+			},
+			{
+				"What is the Capital of Indonesia?" : [ "Jakarta", "Medan",
+						"Surabaya", "Bandung" ]
+			},
+			{
+				"What is the Capital of Iran?" : [ "Tehran", "Isfahan",
+						"Mashhad", "Karaj" ]
+			},
+			{
+				"What is the Capital of Iraq?" : [ "Baghdad", "Mosul", "Basra",
+						"Erbil" ]
+			},
+			{
+				"What is the Capital of Ireland?" : [ "Dublin", "Limerick",
+						"Cork", "Galway" ]
+			},
+			{
+				"What is the Capital of Israel?" : [ "Jerusalem", "Haifa",
+						"Tel Aviv", "Rishon LeZion" ]
+			},
+			{
+				"What is the Capital of Italy?" : [ "Rome", "Naples", "Milan",
+						"Turin" ]
+			},
+			{
+				"What is the Capital of Japan?" : [ "Tokyo", "Osaka",
+						"Yokohama", "Nagoya" ]
+			},
+			{
+				"What is the Capital of Jordan?" : [ "Amman", "Irbid", "Zarqa",
+						"Russeifa" ]
+			},
+			{
+				"What is the Capital of Kazakhstan?" : [ "Astana", "Almaty",
+						"Karagandy", "Shymkent" ]
+			},
+			{
+				"What is the Capital of Kenya?" : [ "Nairobi", "Nakuru",
+						"Mombasa", "Eldoret" ]
+			},
+			{
+				"What is the Capital of North Korea?" : [ "Pyongyang",
+						"Namp’o", "Hamhung", "Hungnam" ]
+			},
+			{
+				"What is the Capital of South Korea?" : [ "Seoul", "Incheon",
+						"Busan", "Daegu" ]
+			},
+			{
+				"What is the Capital of Kuwait?" : [ "Kuwait City",
+						"Al Ahmadi", "Al Jahra", "Hawalli" ]
+			},
+			{
+				"What is the Capital of Libya?" : [ "Tripoli", "Misratah",
+						"Benghazi", "Tarhuna" ]
+			},
+			{
+				"What is the Capital of Madagascar?" : [ "Antananarivo",
+						"Antsirabe", "Toamasina", "Fianarantsoa" ]
+			},
+			{
+				"What is the Capital of Malaysia?" : [ "Kuala Lumpur", "Ipoh",
+						"George Town", "Johor Bahru" ]
+			},
+			{
+				"What is the Capital of Mexico?" : [ "Mexico City",
+						"Iztapalapa", "Ecatepec", "Guadalajara" ]
+			},
+			{
+				"What is the Capital of Mongolia?" : [ "Ulaanbaatar",
+						"Erdenet", "Darkhan", "Khovd" ]
+			},
+			{
+				"What is the Capital of Myanmar?" : [ "Nay Pyi Taw", "Yangon",
+						"Mandalay", "Mawlamyine" ]
+			},
+			{
+				"What is the Capital of Nepal?" : [ "Kathmandu", "Patan",
+						"Pokhara", " Biratnagar" ]
+			},
+			{
+				"What is the Capital of Netherlands?" : [ "Amsterdam",
+						"The Hague", "Rotterdam", "Utrecht" ]
+			},
+			{
+				"What is the Capital of New Zealand?" : [ "Wellington",
+						"Auckland", "Christchurch", "Manukau" ]
+			},
+			{
+				"What is the Capital of Nigeria?" : [ "Abuja", "Lagos", "Kano",
+						"Ibadan" ]
+			},
+			{
+				"What is the Capital of Norway?" : [ "Oslo", "Trondheim",
+						"Bergen", "Stavanger" ]
+			},
+			{
+				"What is the Capital of Oman?" : [ "Muscat", "Seeb", "Salalah",
+						"Bawshar" ]
+			},
+			{
+				"What is the Capital of Pakistan?" : [ "Islamabad", "Karachi",
+						"Lahore", "Faisalabad" ]
+			},
+			{
+				"What is the Capital of Paraguay?" : [ "Asunción",
+						"San Lorenzo", "Ciudad del Este", "Capiatá" ]
+			},
+			{
+				"What is the Capital of Peru?" : [ "Lima", "Callao",
+						"Arequipa", "Trujillo" ]
+			},
+			{
+				"What is the Capital of Philippines?" : [ "Manila",
+						"Quezon City", "Budta", "Davao City" ]
+			},
+			{
+				"What is the Capital of Poland?" : [ "Warsaw", "Wrocław",
+						"Kraków", "Poznań" ]
+			},
+			{
+				"What is the Capital of Portugal?" : [ "Lisbon", "Amadora",
+						"Porto", "Braga" ]
+			},
+			{
+				"What is the Capital of Puerto Rico?" : [ "San Juan",
+						"Carolina", "Bayamón", "Ponce" ]
+			},
+			{
+				"What is the Capital of Qatar?" : [ "Doha", "Al Wakrah",
+						"Al Rayyan", "Al Khor" ]
+			},
+			{
+				"What is the Capital of Romania?" : [ "Bucharest", "Iași",
+						"Cluj-Napoca", "Timișoara" ]
+			},
+			{
+				"What is the Capital of Russia?" : [ "Moscow", "Novosibirsk",
+						"Saint Petersburg", "Yekaterinburg" ]
+			},
+			{
+				"What is the Capital of Saudi Arabia?" : [ "Riyadh", "Mecca",
+						"Jeddah", "Medina" ]
+			},
+			{
+				"What is the Capital of Serbia?" : [ "Belgrade", "Niš",
+						"Novi Sad", "Zemun" ]
+			},
+			{
+				"What is the Capital of Singapore?" : [ "Singapore",
+						"Kuala Lumpur", "Port city", "Singur" ]
+			},
+			{
+				"What is the Capital of South Africa?" : [ "Cape Town",
+						"Johannesburg", "Durban", "Soweto" ]
+			},
+			{
+				"What is the Capital of Spain?" : [ "Madrid", "Valencia",
+						"Barcelona", "Seville" ]
+			},
+			{
+				"What is the Capital of Sri Lanka?" : [ "Colombo",
+						"Maharagama", "Jaffna", "Kandy" ]
+			},
+			{
+				"What is the Capital of Sweden?" : [ "Stockholm", "Malmo",
+						"Gothenburg", "Uppsala" ]
+			},
+			{
+				"What is the Capital of Switzerland?" : [ "Bern", "Zürich",
+						"Geneva", "Basel" ]
+			},
+			{
+				"What is the Capital of Syria?" : [ "Damascus", "Aleppo",
+						"Homs", "Hama" ]
+			},
+			{
+				"What is the Capital of Thailand?" : [ "Bangkok",
+						"Samut Prakan", "Nonthaburi", "Udon Thani " ]
+			},
+			{
+				"What is the Capital of Turkey?" : [ "Ankara", "Istanbul",
+						"Bursa", "Adana" ]
+			},
+			{
+				"What is the Capital of United Kingdom?" : [ "London",
+						"Glasgow", "Birmingham", "Liverpool" ]
+			},
+			{
+				"What is the Capital of United States of America?" : [
+						"Washington D.C.", "New York City", "Los Angeles",
+						"Chicago" ]
+			},
+			{
+				"What is the Capital of Venezuela?" : [ "Caracas", "Maracay",
+						"Maracaibo", "Valencia" ]
+			},
+			{
+				"What is the Capital of Vietnam?" : [ "Hanoi",
+						"Ho Chi Minh City", "Can Tho", "Danang" ]
+			},
+			{
+				"What is the Capital of Zimbabwe?" : [ "Harare", "Chitungwiza",
+						"Bulawayo", "Mutare" ]
+			} ]
+};
